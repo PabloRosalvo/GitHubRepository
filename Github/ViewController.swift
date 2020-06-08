16dfd9de84cb13ceba61f35836daf25a5a3b1069
Pablo Rosalvo <pablo.rosalvo@terceiro-sky.com.br>
2020-06-08 20:46:06 -0300

    Initial Commit

---
+++ Github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by Pablo Rosalvo de Melo Lopes on 08/06/20.
+//  Copyright © 2020 Pablo Rosalvo de Melo Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubTests/GithubTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubTests.swift
+//  GithubTests
+//
+//  Created by Pablo Rosalvo de Melo Lopes on 08/06/20.
+//  Copyright © 2020 Pablo Rosalvo de Melo Lopes. All rights reserved.
+//
+
+import XCTest
+@testable import Github
+
+class GithubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
